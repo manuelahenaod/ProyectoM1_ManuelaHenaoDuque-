67d2d2f33fb954d5df6bca68b0ba161e87271691
Manuela <manuelahenaod19@gmail.com>
2026-04-01 04:55:38 +0200

    Creación contenedor para la paleta y escuchador de click en el botón

---
+++ index.js
@@ -0,0 +1,9 @@
+const boton = document.getElementById("crear");
+const paleta = document.getElementById("paleta");
+
+boton.addEventListener("click", function() {
+
+  const cantidad = document.getElementById("cantidad").value;
+  const formato = document.getElementById("formato").value;
+
+});
