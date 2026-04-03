ee551a62c59ea2cb8d8f775281a7125448e41dbc
Manuela <manuelahenaod19@gmail.com>
2026-04-03 23:39:53 +0200

    Funciones para crear colores aleatorios en formato HSL y HEX

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ boton.addEventListener("click", function() {
   const formato = document.getElementById("formato").value;
 
 });
+
+function colorHSL() {
+  const H= Math.floor(Math.random() * 360);
+  const S= Math.floor(Math.random() * 100);
+  const L= Math.floor(Math.random() * 100); 
+
+  return `hsl(${H}, ${S}%, ${L}%)`;
+
+}
+
+console.log ("color", colorHSL());
+
+function colorHEX() {
+const digitos = "0123456789ABCDEF";
+let color = "#";
+
+for (let i=0; i<6; i++) {
+  color += digitos [Math.floor(Math.random() * 16)]
+};
+
+return color;
+};
+
+console.log ("color", colorHEX());
