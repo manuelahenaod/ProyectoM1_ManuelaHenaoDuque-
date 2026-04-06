bd035ab478b5a4aa6875c97bdb291752532048c1
Manuela <manuelahenaod19@gmail.com>
2026-04-06 03:31:05 +0200

    Función copiar color en portapapeles

--- index.js
+++ index.js
@@ -22,17 +22,22 @@ boton.addEventListener("click", function() {
   fila.style.backgroundColor = color;  //permite ver el color de fondo
   fila.textContent = color;  // permite ver el código del color
 
+  fila.addEventListener("click", () => {
+  navigator.clipboard.writeText(color);
+  mostrarMensaje("Copiado: " + color);
+  });  //Función para copiar color
+
   switch(Number(cantidad)) {
     case 6:
-      fila.style.height = "80px";
+      fila.style.height = "65px";
       break;
 
     case 8:
-      fila.style.height = "60px";
+      fila.style.height = "48px";
       break;
 
     case 9:
-      fila.style.height = "53px";
+      fila.style.height = "43px";
       break;
   }
   
@@ -52,13 +57,22 @@ function colorHSL() {
 
 
 function colorHEX() {
-const digitos = "0123456789ABCDEF";
-let color = "#";
+  const digitos = "0123456789ABCDEF";
+  let color = "#";
 
-for (let i=0; i<6; i++) {
-  color += digitos [Math.floor(Math.random() * 16)]
+  for (let i=0; i<6; i++) {
+    color += digitos [Math.floor(Math.random() * 16)]
+  };
+
+  return color;
 };
 
-return color;
-};
+function mostrarMensaje(texto) {
+  const mensaje = document.getElementById("mensaje");
+  mensaje.textContent = texto;
+  mensaje.style.opacity = "1";
 
+  setTimeout(() => {
+    mensaje.style.opacity = "0";
+  }, 1500);
+}
